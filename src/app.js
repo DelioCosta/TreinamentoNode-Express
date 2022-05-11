a79e7574a9ebe675081f5ef9f483d58405cae3b5
Délio <deliocqcosta@gmail.com>
2022-05-11 20:47:26 -0300

    criação dos métodos get, put, post e delete para API products

--- src/app.js
+++ src/app.js
@@ -1,12 +1,42 @@
 const express = require('express');
+//const ProductsController = require('/controllers/products.js');
 
 const app = express();
 
 const porta = 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
 // app.get('/', (req, res) =>{
 //     res.send(req.query);
 // });
+
+app.get("/products", (req, res) => {
+    res.json([]);
+});
+
+app.post("/products", (req, res) => {
+    res.json(req.body);
+});  
+
+app.get("/products/:id", (req, res) => {
+    const { id } = req.params;
+  
+    res.json({
+      id,
+      name: `Produto ${id}`,
+      price: 300,
+    });
+});
+  
+app.put('/products/:id', function (req, res) {
+    res.json(req.body);
+})
+
+app.delete('/products/:id', function (req, res) {
+    res.send('DELETADO')
+  })
 
 app.get('/:name?', (req, res) =>{
     const {name} = req.params;
