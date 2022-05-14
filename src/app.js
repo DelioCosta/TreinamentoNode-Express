b59571e184be3c48a2ebd65f6efe817f8ba44e83
Délio <deliocqcosta@gmail.com>
2022-05-14 10:28:22 -0300

    Aplicação do MVC e rotas - conteúdo da aula 14/05

--- src/app.js
+++ src/app.js
@@ -1,64 +1,15 @@
-const express = require('express');
-//const ProductsController = require('/controllers/products.js');
+const express = require("express");
+
+const routes = require("./routes");
 
 const app = express();
 
-const porta = 3000;
+const port = 3000;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
+app.use(routes);
 
-// app.get('/', (req, res) =>{
-//     res.send(req.query);
-// });
-
-app.get("/products", (req, res) => {
-    res.json([]);
-});
-
-app.post("/products", (req, res) => {
-    res.json(req.body);
-});  
-
-app.get("/products/:id", (req, res) => {
-    const { id } = req.params;
-  
-    res.json({
-      id,
-      name: `Produto ${id}`,
-      price: 300,
-    });
-});
-  
-// app.put('/products/:id', function (req, res) {
-//     res.json(req.body);
-// });
-
-app.put("/products/:id", (req, res) => {
-    const { id } = req.params;
-  
-    res.json({
-      id,
-      ...(req.body || {}),
-    });
-  });
-  
-
-// app.delete('/products/:id', function (req, res) {    
-// res.send('DELETADO')
-// })
-
-app.delete('/products/:id', function (req, res) {
-
-    res.status(204).send('');
-});
-
-app.get('/:name?', (req, res) =>{
-    const {name} = req.params;
-
-    res.send(`Hello ${name || "world!"}`);
-});
-
-app.listen(porta, () => {
-    console.log(`Servidor executando na porta: ${porta}`);
+app.listen(port, () => {
+  console.log(`Servidor executando na porta: ${port}`);
 });
